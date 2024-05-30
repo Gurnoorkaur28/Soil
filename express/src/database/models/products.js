882be708dfdc0553725a858204b5b831fbae63da
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2024-05-31 00:29:28 +1000

    Changed product model to auto increment id insted of manually adding

--- express/src/database/models/products.js
+++ express/src/database/models/products.js
@@ -1,26 +1,30 @@
 module.exports = (sequelize, DataTypes) =>
-  sequelize.define("products", {
-    id:{
-      type: DataTypes.INTEGER,
-        primaryKey: true
+  sequelize.define(
+    "products",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      timestamps: false,
     }
-   
-  }, {
-    timestamps: false
-  });
+  );
--- express/src/database/seedData.js
+++ express/src/database/seedData.js
@@ -3,126 +3,108 @@ async function seedProducts(db) {
   if (productCount === 0) {
     await db.product.bulkCreate([
       {
-        id: 1,
         name: "milk",
         price: 3.5,
         description: "1 litre, Fresh Cow milk",
         image: "milk.png",
       },
       {
-        id: 2,
         name: "Tomatoes",
         price: 4.0,
         description: "1kg, Juicy Tomatoes",
         image: "tomatoes.png",
       },
       {
-        id: 3,
         name: "Carrots",
         price: 1.5,
         description: "1kg, Fresh Carrots",
         image: "carrot.png",
       },
       {
-        id: 4,
         name: "Spinach",
         price: 2.0,
         description: "Per pack",
         image: "spinach.png",
       },
       {
-        id: 5,
         name: "Broccoli",
         price: 2.5,
         description: "each",
         image: "broccoli.png",
       },
       {
-        id: 6,
         name: "Potatoes",
         price: 5.5,
         description: "1 kg",
         image: "potatoes.png",
       },
       {
-        id: 7,
         name: "Onions",
         price: 4.0,
         description: "Per kg",
         image: "onions.png",
       },
       {
-        id: 8,
         name: "Soya Milk",
         price: 3.5,
         description: "1 litre (lactose free)",
         image: "soya-milk.png",
       },
       {
-        id: 9,
         name: "Pulses",
         price: 4.0,
         description: "Per kg",
         image: "pulses.png",
       },
       {
-        id: 10,
         name: "Flour",
         price: 4.9,
         description: "Per kg",
         image: "flour.png",
       },
       {
-        id: 11,
         name: "Almond",
         price: 9.0,
         description: "Per kg",
         image: "almond.png",
       },
       {
-        id: 12,
         name: "Eggs",
         price: 9.0,
         description: "12 Cage Free Eggs",
         image: "egg.png",
       },
       {
-        id: 13,
         name: "Blueberries",
         description: "Special discount on blueberries!",
         price: 4.05,
         image: "Blueberries.png",
       },
       {
-        id: 14,
         name: "Cottage Cheese",
         description: "Special offer on cottage cheese (per pack)",
         price: 5.55,
         image: "CottageCheese.png",
       },
       {
-        id: 15,
         name: "Strawberries",
         description: "Special deal on strawberries!",
         price: 4.05,
         image: "Strawberries.png",
       },
       {
-        id: 16,
         name: "Avocado",
         description: "Limited time offer on fresh avocados",
         price: 2.1,
         image: "Avocado.png",
       },
       {
-        id: 17,
         name: "Apples",
         description: "Fresh and crisp apples, great for snacking.",
         price: 3.05,
         image: "Apples.png",
       },
       {
-        id: 18,
         name: "Bananas",
         description: "Fresh and sweet bananas",
         price: 4.0,
@@ -131,38 +113,7 @@ async function seedProducts(db) {
     ]);
   }
 }
-// async function seedSpecialProducts(db) {
-//   await db.specialProduct.bulkCreate([
-//     {
-//       id: 16,
-//       special_id: 1,
-//       discounted_price: 1.5,
-//       start_date: "2024-05-12",
-//       end_date: "2024-06-18",
-//     },
-//     {
-//       id: 15,
-//       special_id: 2,
-//       discounted_price: 3.05,
-//       start_date: "2024-05-12",
-//       end_date: "2024-06-18",
-//     },
-//     {
-//       id: 14,
-//       special_id: 3,
-//       discounted_price: 4.05,
-//       start_date: "2024-05-12",
-//       end_date: "2024-06-18",
-//     },
-//     {
-//       id: 13,
-//       special_id: 4,
-//       discounted_price: 3.05,
-//       start_date: "2024-05-12",
-//       end_date: "2024-06-18",
-//     },
-//   ]);
-// }
+
 async function seedSpecialProducts(db) {
   const specialProductCount = await db.specialProduct.count();
   if (specialProductCount === 0) {
@@ -275,13 +226,13 @@ async function seedreview(db) {
       {
         rating: 3,
         productId: 1,
-        comment:"good quality",
+        comment: "good quality",
         user_id: 1,
       },
       {
         rating: 3,
         productId: 2,
-        comment:"good quality",
+        comment: "good quality",
         user_id: 1,
       },
     ]);
